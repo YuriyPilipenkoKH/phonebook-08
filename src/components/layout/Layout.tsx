c698b715e72d6b2ea0781d0a7b236a80ef882706
Yuriy Pilipenko <yurik2061@gmail.com>
2025-03-01 19:13:24 +0200

    modal works

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,36 +1,54 @@
 import { Suspense } from 'react'
 import { MainFooter, MainHeader } from './Layout.styled'
 import { Outlet } from 'react-router-dom'
-
+import ThemeChanger from '../button/ThemeChanger'
+import LangChanger from '../button/LangChanger'
+import MainModal from '../modals/MainModal'
+import { useAll } from '../../hooks/useAll'
+import { useLanguage } from '../../hooks/useLanguage'
+import { useAppDispatch } from '../../hooks/useAppDispatch'
+import { onModalOpen } from '../../redux/modal/modalSlice'
+import { fakeContact } from '../../data/contact'
 
 
 
 
 const Layout = () => {
-//   const lang = useLanguage()
-//     const dispatch = useAppDispatch();
+  const {  modalIsOpen , selectedContact } = useAll()
+  const lang = useLanguage()
+    const dispatch = useAppDispatch();
     
 // const quit =() => {
 //   dispatch(logOut( 'Dude'))
 // }
+const click = () => {
+  dispatch(onModalOpen(fakeContact))
+}
     return (
     <>
       <MainHeader  className="main-header" >
-        {/* <AppBar/> */} header there
+        {/* <AppBar/> */} <p>header there</p>
+        <ThemeChanger/>
+        <LangChanger/>
+        <button type='button' onClick={click}>Mo</button>
       </MainHeader>
         <Suspense >
             <Outlet />
         </Suspense>
         <MainFooter >
-          {/* {lang.footerTitle}  */}
+          {lang.footerTitle} 
           {/* <button 
           className='quit'
           type='button' 
           onClick={quit}>—</button>
           {'2025'} */}
           {/* < MirrorStreamIcon /> */}
-          shocker there
+
         </MainFooter>
+
+    { modalIsOpen && selectedContact && (
+      <MainModal contact={selectedContact}/>
+      ) }
       </>
     )}
 
